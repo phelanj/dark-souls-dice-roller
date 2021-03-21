448c66be791a1a0b3ec6798840fce56a7a82a12e
Jack Phelan <phelanj@users.noreply.github.com>
2021-03-21 14:44:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,133 @@
+let dieDisplay = document.getElementById("die-display");
+let resultDisplay = document.getElementById("results-display");
+let total = 0;
+let totalOutput = "";
+let re = /= 0/;
+
+let dice = [{
+        type: "Black Die",
+        rollArray: [0, 1, 1, 1, 2, 2],
+        count: 0,
+        result: ""
+    },
+    {
+        type: "Blue Die",
+        rollArray: [1, 2, 2, 3, 3, 3],
+        count: 0,
+        result: ""
+    },
+    {
+        type: "Orange Die",
+        rollArray: [2, 2, 2, 3, 3, 4],
+        count: 0,
+        result: ""
+    },
+    {
+        type: "Roll Die",
+        rollArray: [0, 1],
+        count: 0,
+        result: ""
+    }
+];
+
+function addDie(input) {
+    checkIfClearNeeded(input);
+    if (total != 0 || re.exec(totalOutput)) {
+        clearResults();
+    }
+    dice[input]["count"]++;
+    displayDice();
+}
+
+function getResults() {
+    if (total == 0 && !re.exec(totalOutput)) {
+        performRandom();
+        addResults();
+    }
+}
+
+function displayDice() {
+    let output = "";
+    let die;
+    let first = true;
+    for (die in dice) {
+        dieCount = dice[die]["count"];
+        dieType = dice[die]["type"];
+        if (dieCount > 0) {
+            if (!first) {
+                output += " + ";
+            }
+            output += String(dieCount) + " " + dieType + " ";
+            first = false;
+        }
+    }
+    dieDisplay.value = output;
+}
+
+function performRandom() {
+    let die;
+    let count;
+    for (die in dice) {
+        for (count = 0; count < dice[die]["count"]; count++) {
+            var rollArray = dice[die]["rollArray"];
+            currentResult = dice[die]["result"];
+            var randomElement = _.sample(rollArray);
+            if (currentResult) {
+                currentResult += " + ";
+            }
+            currentResult += String(randomElement);
+            dice[die]["result"] = currentResult;
+        }
+    }
+}
+
+function addResults() {
+    let die;
+    let dieTotal = 0;
+    let output = "";
+    let first = true;
+    let display = document.getElementById("results-display");
+    for (die in dice) {
+        let dieResult = dice[die]["result"];
+        if (dieResult) {
+            dieTotal += eval(dieResult);
+            if (!first) {
+                output += " + ";
+            }
+            output += " ( " + dieResult + " ) ";
+            first = false;
+        }
+    }
+    output += " = " + String(dieTotal);
+    totalOutput = output;
+    total = dieTotal;
+    resultDisplay.value = output;
+}
+
+function clearResults() {
+    let die;
+    for (die in dice) {
+        dice[die]["count"] = 0;
+        dice[die]["result"] = "";
+    }
+    dieDisplay.value = "";
+    resultDisplay.value = "";
+    total = 0;
+    totalOutput = "";
+}
+
+function checkIfClearNeeded(input) {
+    let die;
+    let block;
+    if (input == 3) {
+        for (die = 0; die < dice.length - 1; die++) {
+            if (dice[die]["count"] > 0) {
+                clearResults();
+            }
+        }
+    } else {
+        if (dice[3]["count"] > 0) {
+            clearResults();
+        }
+    }
+}
